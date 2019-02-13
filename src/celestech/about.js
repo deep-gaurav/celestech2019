36dd3d96891a4a8204f08b71a16b61dcf0a9088f
Aastha <aasthagoel2@gmail.com>
2019-02-13 17:00:17 +0530

    add about

--- src/celestech/about.js
+++ src/celestech/about.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
-import { Paper, Card, CardActionArea, CardMedia, CardContent, Typography, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from '@material-ui/core';
+import { Paper, Card, Grid, CardActionArea, CardMedia, CardContent, Typography, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from '@material-ui/core';
+
+import collegeImage from './res/aboutImages/college.jpg';
 
 class About extends Component{
 
     detail=[
         {
-            "title":"Celestech",
+            "header":"About Aryabhatta College",
+            "detail":<div>
+                <p>ARYABHATTA COLLEGE comes into existence from the academic session 2014-15. It is an institution directly maintained by the University of Delhi. The college was formerly known as Ram Lal Anand College (Evening) and was established in 1973.
+
+The working hours at the Aryabhatta College are 8:30 a.m. to 5:00 p.m. Classes shall commence at 8:30 a.m.
+
+The College is located adjacent to the South Campus of the University of Delhi in the picturesque surroundings of the South Delhi section of the the Aravali range.
+
+Aryabhatta College is a co-educational institution and conducts Humanities, Arts and Commerce courses at the undergraduate level. There are presently around 2200 students on the rolls of the College.</p>
+            </div>,
+            "image":collegeImage
+        },
+        {
+            
             "header":"About the fest",
             "detail":"LOTIDHVkjfdhbkjxnb vc",
             "image":false
@@ -19,30 +34,27 @@ class About extends Component{
         return(
             <div style={{padding:"10px",paddingTop:"4em", backgroundColor:"black"}}>
                 <Paper>
+                    <Grid container direction="column" justify="center" alignItems="center">
                     {
                         this.detail.map(
                             (ab1,index)=>(
-                                <Card>
-                                    <CardActionArea>
-                                        <CardMedia image={ab1.image}/>
+                                <Grid item >
+                                    <Card style={{maxWidth:"75vw", margin:"8px"}}>
+                                    
+                                        <CardMedia src={ab1.image} component="img"/>
                                         <CardContent>
                                             <Typography variant='title'>
-                                                {ab1.title+" - "+ab1.header}
+                                                {ab1.header}
                                             </Typography>
-<ExpansionPanel defaultExpanded>
-    <ExpansionPanelSummary>
-        description
-    </ExpansionPanelSummary>
-    <ExpansionPanelDetails>
-        {ab1.detail}
-    </ExpansionPanelDetails>
-</ExpansionPanel>
+
+                                            {ab1.detail}
                                         </CardContent>
-                                    </CardActionArea>
-                                </Card>
+                                    </Card>
+                                </Grid>
                             )
                         )
                     }
+                    </Grid>
                 </Paper>
             </div>
         )
