72b77dac1102161d106cd1e596eaa6c137421e61
Deep <deep@pop-os.localdomain>
2019-02-20 01:18:09 +0530

    add sponsors

--- src/celestech/sponsor.js
+++ src/celestech/sponsor.js
@@ -1,12 +1,60 @@
 import React, {Component} from 'react';
 
+import aspenLogo from './res/sponsors/aspen.jpg';
+import grabonLogo from './res/sponsors/grabon.jpg';
+import landmarkLogo from './res/sponsors/landmark.jpg';
+import midlandLogo from './res/sponsors/midland.png';
+import uniqueLogo from './res/sponsors/unique.gif';
+import { Paper } from '@material-ui/core';
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+ 
+
 class Sponsor extends Component{
+
+    sponsors = [
+        {
+            "name":"Aspen Research Services",
+            "logo":aspenLogo
+        },
+        {
+            "name":"GrabOn",
+            "logo":grabonLogo
+        },
+        {
+            "name":"Landmark Institue",
+            "logo":landmarkLogo
+        },
+        {
+            "name":"Midland - The Book Shop",
+            "logo":midlandLogo
+        },
+        {
+            "name":"Unique Siksha",
+            "logo":uniqueLogo
+        }
+    ]
 
     render() {
 
         return(
-            <div>
-                Sponsor
+            <div style={{paddingTop:"4em"}}>
+                <Paper>
+                    <Carousel
+                        showArrows
+                        infiniteLoop
+                        autoPlay
+                        stopOnHover={false}
+                    >
+                        {this.sponsors.map((sponsor,index)=>(
+                            <div>
+                                <img src={sponsor.logo}/>
+                                <h2 className="legend">{sponsor.name}</h2>
+                            </div>
+                        ))}
+                    </Carousel>
+                </Paper>
             </div>
         )
     }
